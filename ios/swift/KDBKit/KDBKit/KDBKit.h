6cd14363771e1a83dc509cd8c9a8dd99be03990e
yulu.kong <yulu.kong@ubtrobot.com>
2020-04-07 14:50:34 +0800

    init project

---
+++ ios/swift/KDBKit/KDBKit/KDBKit.h
@@ -0,0 +1,19 @@
+//
+//  KDBKit.h
+//  KDBKit
+//
+//  Created by yulu kong on 2020/4/7.
+//  Copyright © 2020 yulu kong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KDBKit.
+FOUNDATION_EXPORT double KDBKitVersionNumber;
+
+//! Project version string for KDBKit.
+FOUNDATION_EXPORT const unsigned char KDBKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KDBKit/PublicHeader.h>
+
+
